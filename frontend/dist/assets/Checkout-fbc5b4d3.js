46a9d7a1f4fe795c8f14fa0165328e7b27956e99
phuocduydev <phuocduy.dev@gmail.com>
2023-03-25 21:20:55 +0700

    add build folder

---
+++ frontend/dist/assets/Checkout-fbc5b4d3.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-6b6d889c.js";const o=()=>t("div",{children:"Checkout"});export{o as default};
---
+++ frontend/dist/assets/plus-9f102ee5.js
@@ -0,0 +1,1 @@
+const s="/assets/minus-5cfecfe2.svg",c="/assets/plus-0bb71164.svg";export{s as m,c as p};
