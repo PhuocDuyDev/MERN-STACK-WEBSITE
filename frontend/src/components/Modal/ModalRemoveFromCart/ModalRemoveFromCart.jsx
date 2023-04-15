4ca7d7b6dbff9c1b9bfb4a4b147a8ddaa6b21376
phuocduydev <phuocduy.dev@gmail.com>
2023-04-15 07:13:19 +0700

    done modal remove from cart

--- frontend/src/components/Modal/ModalRemoveFromCart/ModalRemoveFromCart.jsx
+++ frontend/src/components/Modal/ModalRemoveFromCart/ModalRemoveFromCart.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import ReactDom from 'react-dom';
+import styles from './ModalRemoveFromCart.module.css';
 
-const ModalRemoveFromCart = () => {
-  return (
-    <div>ModalRemoveFromCart</div>
-  )
-}
+const ModalRemoveFromCart = ({ open, onClose, removeFromCart }) => {
+    if (!open) return null;
 
-export default ModalRemoveFromCart
+    return ReactDom.createPortal(
+        <>
+            <div className={styles['overlay']} onClick={onClose} />
+            <div
+                className={`container ${styles['modal-remove-from-cart']}`}
+                onClick={(e) => {
+                    e.stopPropagation();
+                }}
+            >
+                <div className={`${styles['modal-container']}`}>
+                    <button
+                        type='button'
+                        onClick={onClose}
+                        className={styles['btn-close']}
+                    >
+                        X
+                    </button>
+                    <h2 className='section-heading'>Are you sure?</h2>
+                    <p>You are already to delete items from cart.</p>
+                    <div className={styles['action']}>
+                        <button
+                            className={`${styles['btn-cancel']} btn`}
+                            onClick={onClose}
+                            type='button'
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            type='button'
+                            className={`${styles['btn-cancel']} btn`}
+                            onClick={() => {
+                                removeFromCart();
+                                onClose();
+                            }}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>,
+        document.getElementById('portal')
+    );
+};
+
+export default ModalRemoveFromCart;
