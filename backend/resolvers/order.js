8ac9609c96fd5b070b0cfeaa025a5996379419b0
phuocduydev <phuocduy.dev@gmail.com>
2023-04-15 07:43:58 +0700

    add order resolver (not done

--- backend/resolvers/order.js
+++ backend/resolvers/order.js
@@ -40,46 +40,41 @@ const orderResolvers = {
         },
     },
     Mutation: {
-        async createOrder(_, { products }, { userId }) {
+        async createOrder(_, { products }, { currentUser }) {
             // Verify that the user is authenticated
-            if (!userId) {
-                throw new Error('You must be logged in to create an order');
-            }
-
-            // Look up the user who placed the order
-            const user = await User.findById(userId);
-            if (!user) {
-                throw new Error('Invalid user ID');
-            }
-
-            // Verify that all products are valid
-            const productIds = products.map(({ productId }) => productId);
-            const validProducts = await Product.find({
-                _id: { $in: productIds },
-            });
-            if (validProducts.length !== productIds.length) {
-                throw new Error('Invalid product ID');
-            }
-
-            // Calculate the total cost of the order
-            const total = validProducts.reduce(
-                (acc, { price }, i) => acc + price * products[i].quantity,
-                0
-            );
-
-            // Create the new order document
-            const order = new Order({
-                user: userId,
-                products: products.map(({ productId, quantity }) => ({
-                    product: productId,
-                    quantity,
-                })),
-                total,
-            });
-
-            // Save the order and return it
-            await order.save();
-            return order;
+            // if (!currentUser) {
+            //     throw new Error('You must be logged in to create an order');
+            // }
+            // // Look up the user who placed the order
+            // const user = await User.findById(currentUser);
+            // if (!user) {
+            //     throw new Error('Invalid user ID');
+            // }
+            // // Verify that all products are valid
+            // const productIds = products.map(({ productId }) => productId);
+            // const validProducts = await Product.find({
+            //     _id: { $in: productIds },
+            // });
+            // if (validProducts.length !== productIds.length) {
+            //     throw new Error('Invalid product ID');
+            // }
+            // // Calculate the total cost of the order
+            // const total = validProducts.reduce(
+            //     (acc, { price }, i) => acc + price * products[i].quantity,
+            //     0
+            // );
+            // // Create the new order document
+            // const order = new Order({
+            //     user: userId,
+            //     products: products.map(({ productId, quantity }) => ({
+            //         product: productId,
+            //         quantity,
+            //     })),
+            //     total,
+            // });
+            // // Save the order and return it
+            // await order.save();
+            // return order;
         },
         updateOrderStatus: async (_, { orderId, status }, { user }) => {
             if (!user) {
