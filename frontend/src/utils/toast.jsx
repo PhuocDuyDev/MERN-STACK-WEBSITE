0011321474a8bf2d5e519603f57f5ad9af2b4338
phuocduydev <phuocduy.dev@gmail.com>
2023-04-15 07:28:13 +0700

    remove toast time default and export from const folder

--- frontend/src/utils/toast.jsx
+++ frontend/src/utils/toast.jsx
@@ -1,18 +1,19 @@
 import { toast } from 'react-toastify';
-const TIME_DEFAULT = 1.5;
-export const notifyWarning = (errMsg, timeOut = TIME_DEFAULT) =>
+import { TIME_SHOW_TOAST_DEFAULT } from '../const';
+
+export const notifyWarning = (errMsg, timeOut = TIME_SHOW_TOAST_DEFAULT) =>
     toast.warning(errMsg, {
         autoClose: timeOut * 1000,
         closeOnClick: true,
     });
 
-export const notifyInfo = (infoMsg, timeOut = TIME_DEFAULT) =>
+export const notifyInfo = (infoMsg, timeOut = TIME_SHOW_TOAST_DEFAULT) =>
     toast.info(infoMsg, {
         autoClose: timeOut * 1000,
         closeOnClick: true,
     });
 
-export const notifySuccess = (successMsg, timeOut = TIME_DEFAULT) =>
+export const notifySuccess = (successMsg, timeOut = TIME_SHOW_TOAST_DEFAULT) =>
     toast.success(successMsg, {
         autoClose: timeOut * 1000,
         closeOnClick: true,
