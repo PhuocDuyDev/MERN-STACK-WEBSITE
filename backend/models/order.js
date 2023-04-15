cd7678a85afeb14e21b6b1e18f710fde9a475d36
phuocduydev <phuocduy.dev@gmail.com>
2023-04-15 07:43:29 +0700

    add model order

--- backend/models/order.js
+++ backend/models/order.js
@@ -14,10 +14,18 @@ const orderSchema = new mongoose.Schema(
                     ref: 'Product',
                     required: true,
                 },
+                size: {
+                    type: String,
+                    required: true,
+                },
                 quantity: {
                     type: Number,
                     required: true,
                     default: 1,
+                },
+                price: {
+                    type: Number,
+                    required: true,
                 },
             },
         ],
