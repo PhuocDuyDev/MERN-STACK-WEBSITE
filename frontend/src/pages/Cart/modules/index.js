ce69671e97bcaf92c6c3fb96ff5040251f7fb0d1
phuocduydev <phuocduy.dev@gmail.com>
2023-04-15 07:36:56 +0700

    export new module for Cart: FormCheckout

--- frontend/src/pages/Cart/modules/index.js
+++ frontend/src/pages/Cart/modules/index.js
@@ -2,3 +2,4 @@ export { default as CartList } from './CartList/CartList';
 export { default as CartItem } from './CartItem/CartItem';
 export { default as SummaryList } from './SummaryList/SummaryList';
 export { default as SummaryItem } from './SummaryItem/SummaryItem';
+export { default as FormCheckOut } from './FormCheckOut/FormCheckOut';
